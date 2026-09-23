ad5e037a7b01e4f60f109f2dd0ff6314239ec796
Ananya Virwani <ananyavirwani255@gmail.com>
2026-09-23 18:49:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function firstWord(s) {
   // your code here
-	let arr=s.split(" ")
-	return arr[0]
-
+	
 	if(s==''){
 		return ''
 	}
+	let arr=s.split(" ")
+	return arr[0]
+
 }
 
 // Do not change the code below
