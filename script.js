8e6ec989e244b961bca2d6149c3d78ca8ccc2b43
Ananya Virwani <ananyavirwani255@gmail.com>
2026-09-23 18:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	let arr=str.split(" ")
+	console.log(arr[0])
+
+	if(s==''){
+		return ''
+	}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
